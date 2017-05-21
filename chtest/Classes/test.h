1d135a369f5d087be3fcdfee0f3449cdfe1f1585
bamcihc <343658984@qq.com>
2017-05-21 21:50:22 +0800

    11

---
+++ chtest/Classes/test.h
@@ -0,0 +1,13 @@
+//
+//  test.h
+//  chtest
+//
+//  Created by bamcihc on 2017/5/21.
+//  Copyright © 2017年 nian. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface test : NSObject
+-(void)test;
+@end
---
+++ chtest/Classes/test.m
@@ -0,0 +1,18 @@
+//
+//  test.m
+//  chtest
+//
+//  Created by bamcihc on 2017/5/21.
+//  Copyright © 2017年 nian. All rights reserved.
+//
+
+#import "test.h"
+
+@implementation test
+-(void)test
+{
+
+    NSLog(@"nsnsn");
+
+}
+@end
